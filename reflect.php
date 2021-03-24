4126a48fec1585d1459d293a878cfffe886876ac
jkf <git@spamriot.com>
2021-03-24 11:47:44 -0700

    cleaner output; more header magic

--- reflect.php
+++ reflect.php
@@ -1,64 +1,206 @@
 <?php
 
-function dump($data) {
-    echo "<pre>";
-    echo print_r($data, true);
-    echo "</pre>";
+// spill our guts to the world if something isn't right
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+xdebug_disable();  // too much noise by default
+
+
+function getHeader($name, $default = null) {
+    $serverKey = str_replace('-', '_', strtoupper($name));
+    if (isset($_SERVER["HTTP_$serverKey"])) {
+        return $_SERVER["HTTP_$serverKey"];
+    }
+    return $default;
 }
 
-$body = stream_get_contents(fopen('php://input', 'r'));
+
+function cleanTrace($stackTrace) {
+    $stack = array();
+    // hide internal frames
+    array_pop($stackTrace);
+    foreach ($stackTrace as $frame) {
+        $line = '';
+        if (isset($frame['file'])) {
+            $line .= $frame['file'] . ' ';
+        }
+        if (isset($frame['line'])) {
+            $line .= $frame['line'] . ' ';
+        }
+        if (isset($frame['class'])) {
+            $line .= ' ' . $frame['class'] . $frame['type'];
+        }
+        $line .= $frame['function'] . '(';
+        if (count($frame['args'])) {
+            // don't return the actual args, but the types are nice
+            $args = array();
+            foreach ($frame['args'] as $arg) {
+                $args[] = gettype($arg);
+            }
+            $line .= join(', ', $args);
+        }
+        $line .= ')';
+        $stack[] = $line;
+    }
+    return $stack;
+}
+
+function fail($ex, $spillage = null) {
+    $resp = array(
+        'success' => false,
+        'error' => $ex->getMessage()
+    );
+    if ($spillage !== null) {
+        $resp['spillage'] = $spillage;
+    }
+    $resp['data'] = array(
+        'backtrace' => cleanTrace($ex->getTrace())
+    );
+    echo json_encode($resp, JSON_PRETTY_PRINT);
+    exit(1);
+}
+
+
+class HttpReflect {
+    private $body = null;
+    private $meta = [];
+
+    public function __construct() {
+        $this->body = stream_get_contents(fopen('php://input', 'r'));
+        // TODO: use sorted copies
+        ksort($_ENV);
+        ksort($_GET);
+        ksort($_POST);
+        ksort($_REQUEST);
+        ksort($_SERVER);
+    }
+
+    public function getStatusCode($default) {
+        return getHeader('x-refl-status', $default);
+    }
+
+    public function sendSessionHeaders() {
+        global $SESSION_STATUS;
+        // e.g. a "slug", optionally preceeded with +- to customize behavior
+        $sessionId = getHeader('x-refl-session', 'default');
+        // assume new session by default
+        if (!preg_match('/^\w/', $sessionId)) {
+            $keyOp = substr($sessionId, 0, 1);
+            $sessionId = substr($sessionId, 1);
+        } else {
+            $keyOp = '+';
+        }
+        session_name('session');
+        session_id($sessionId);
+        session_start();
+
+        if ($keyOp == '-') {
+            $sessionStatus = session_status();
+            if (isset($_SESSION['id'])) {
+                session_destroy();
+                $this->meta['session'] = 'destroyed';
+            } else {
+                $this->meta['session'] = 'did not exist';
+            }
+        } else if ($keyOp == '?') {
+            // just avoid doing anything
+        } else if ($keyOp == '+') {
+            $_SESSION['id'] = $sessionId;
+            if (!isset($_SESSION['started_at'])) {
+               $_SESSION['started_at'] = time();
+            }
+            if (!isset($_SESSION['counter'])) {
+               $_SESSION['counter'] = 1;
+            } else {
+               $_SESSION['counter'] += 1;
+            }
+        } else {
+            throw new Exception("Invalid session key: $sessionId");
+        }
+    }
+
+    private function getRequestHeaders() {
+        $headers = [];
+        foreach ($_SERVER as $key => $val) {
+            if (substr($key, 0, 5) === 'HTTP_') {
+                $nameParts = explode('_', substr($key, 5));
+                // CGI QQ
+                $headers[strtolower(implode('-', $nameParts))] = $val;
+            }
+        }
+        return $headers;
+    }
+
+    public function processRequest() {
+        // misc schenanigans
+        $delaySecs = floatval(getHeader('x-refl-delay', 0));
+        if ($delaySecs > 0) {
+            $this->meta['delay'] = $delaySecs;
+            usleep(intval($delaySecs * 1000000));
+        }
+    }
+
+    public function getReflection($startTime) {
+        $defaultData = [
+            'cookies' => $_COOKIE,
+            'files' => $_FILES,
+            'meta' => $this->meta,
+            'session' => $_SESSION,
+            'headers' => $this->getRequestHeaders(),
+            'body' => $this->body
+        ];
+        $resp = [];
+        foreach ($defaultData as $key => $val) {
+            if (!empty($val)) {
+                $resp[$key] = $val;
+            }
+        }
+        $resp['request'] = [
+            'src_addr' => $_SERVER['REMOTE_ADDR'],
+            'src_port' => $_SERVER['REMOTE_PORT'],
+            'method' => $_SERVER['REQUEST_METHOD'],
+            'url' => "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+            'duration_sec' => floatval(preg_replace(
+                '/^(\d+\.[0]+[^0]).*/',
+                '\1',
+                microtime(true) - $startTime,
+            ))
+        ];
+        return $resp;
+    }
+}
+
+
+// == HEADERS =================================================================
 if ($_SERVER['HTTP_ACCEPT'] == 'application/json') {
-    $data = [
-        'COOKIE' => $_COOKIE,
-        'ENV' => $_ENV,
-        'FILES' => $_FILES,
-        'GET' => $_GET,
-        'POST' => $_POST,
-        'REQUEST' => $_REQUEST,
-        'SERVER' => $_SERVER,
-    ];
-    if (isset($_SESSION)) {
-        $data['SESSION'] = $_SESSION;
-    }
-    if ($body) {
-        $data['body'] = $body;
-    }
     header('Content-Type: application/json');
-    echo json_encode($data, JSON_PRETTY_PRINT);
-
 } else {
-    echo "<html><body><pre>";
-
-    echo '<h2>$_GET</h2>';
-    dump($_GET);
-
-    echo '<h2>$_POST</h2>';
-    dump($_POST);
-
-    echo '<h2>$_SERVER</h2>';
-    dump($_SERVER);
-
-    echo '<h2>$_FILES</h2>';
-    dump($_FILES);
-
-    echo '<h2>$_COOKIE</h2>';
-    dump($_COOKIE);
-
-    echo '<h2>$_REQUEST</h2>';
-    dump($_REQUEST);
-
-    echo '<h2>$_ENV</h2>';
-    dump($_ENV);
-
-    if (isset($_SESSION)) {
-        echo '<h2>$_SESSION</h2>';
-        dump($_SESSION);
-    }
-
-    if ($body) {
-        echo '<h2>Request Body<h2>';
-        echo "<pre>$body</pre>";
-    }
-
-    echo "</pre></body></html";
+    header('Content-Type: text/plain');
 }
+ob_start();
+
+
+// == EXTRA HEADERS + BODY ====================================================
+$error = null;
+$startTime = microtime(true);
+try {
+    $ref = new HttpReflect();
+    http_response_code($ref->getStatusCode(200));
+    $ref->sendSessionHeaders();
+    $ref->processRequest();
+    $response = $ref->getReflection($startTime);
+} catch (Exception $e) {
+    $error = $e;
+}
+
+$spillage = ob_get_contents();
+ob_end_clean();
+if ($spillage || $error) {
+    if (!$error) {
+        $error = new Exception('API Spillage');
+    }
+    fail($error, $spillage);
+}
+
+echo json_encode($response, JSON_PRETTY_PRINT);
